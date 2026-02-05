e4590b978274fd43aea92655c894ffb01b4d9989
Aidan Mcclain <qjzxv123@gmail.com>
2026-02-05 11:10:49 -0500

    Add MakeMultiFilter and TemplateProcessor classes

---
+++ template-processor.js
@@ -0,0 +1,14 @@
+class TemplateProcessor {
+    constructor(template) {
+    this.template  = template ;
+  }
+  fillIn(dictionary){
+        let returnString = this.template;
+        for (const property in dictionary){
+            if (Object.hasOwn(dictionary, property)) {
+                returnString = returnString.replaceAll("{{" + property + "}}",dictionary[property]);
+            }
+        }
+        return returnString.replaceAll(/{{.*}}/g, "");
+    }
+}
